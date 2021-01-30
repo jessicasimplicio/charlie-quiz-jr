12c1940356c84b9136c831f4d073713c260dcf7c
Jessica Simplicio <jessicasimplicio1@gmail.com>
2021-01-30 01:01:39 -0300

    feat: add font

--- pages/_app.js
+++ pages/_app.js
@@ -29,6 +29,10 @@ const theme = db.theme
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Acme&family=Yusei+Magic&display=swap" rel="stylesheet" />
+      </Head>
       <ThemeProvider theme={theme}>
       <GlobalStyle />
         <Component {...pageProps} />
